603002b9ddfc1c04f0f6dd060fd1c7e3f292e479
FedorEvropov <f.europov@yandex.ru>
2021-06-25 10:55:38 +0300

    Update MyFP.java

--- sandbox/src/main/java/ru/stqa/ft/sandbox/MyFP.java
+++ sandbox/src/main/java/ru/stqa/ft/sandbox/MyFP.java
@@ -4,8 +4,19 @@ public class MyFP {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+
+		System.out.println(2+2);
+		System.out.println(2*2);
+		System.out.println(2/2);
+		System.out.println(2-2);
+		System.out.println(1/2);
+		System.out.println(1.0/2);
+		System.out.println(2/2.0);
+		System.out.println("2"+"2");
+		System.out.println("2"+2);
     }
 }
+
 
 
 		
